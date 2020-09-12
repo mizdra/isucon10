81384896477bb5e45246efa6e62be6dc0660b584
mizdra <pp.mizdra@gmail.com>
2020-09-12 16:57:29 +0900

    env.shもデプロイできるように

--- sokkyou/deploy_backend.sh
+++ sokkyou/deploy_backend.sh
@@ -6,6 +6,7 @@ echo "⚡️ Deploy backend"
 for REMOTE in ${BACKEND[@]}; do
   echo "🚀 deploy ($REMOTE $USER)"
   rsync -avz --exclude-from=.gitignore --exclude='.git' -e 'ssh' . isucon@$REMOTE:/home/isucon/isuumo/webapp/
+  RSYNC env.sh /home/isucon/env.sh
 
   ssh isucon@$REMOTE 'export PATH=/home/isucon/local/node/bin:$PATH && cd /home/isucon/isuumo/webapp/nodejs && /home/isucon/local/node/bin/npm install && sudo systemctl restart isuumo.nodejs.service && sudo sysctl -p'
 done
