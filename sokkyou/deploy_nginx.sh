b097e247aec11d00e2bebb73c01b2572bb6f1dd2
mizdra <pp.mizdra@gmail.com>
2020-09-12 16:55:45 +0900

    nginxのデプロイコマンドを改善

--- sokkyou/deploy_nginx.sh
+++ sokkyou/deploy_nginx.sh
@@ -3,5 +3,6 @@ source ./sokkyou/sokkyou.sh
 
 for REMOTE in ${NGINX[@]}; do
   RSYNC conf/nginx/nginx.conf /etc/nginx/nginx.conf
+  RSYNC conf/nginx/sites-available/isuumo.conf /etc/nginx/sites-available/isuumo.conf
   ssh $REMOTE "sudo systemctl restart nginx"
 done
